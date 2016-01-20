cd9fbc437223652eb27a280d8189ab97533ce277
thetime <thetime2@gmx.com>
2016-01-20 20:03:19 +0000

    working prototype

---
+++ tests/test_discord_bot.py
@@ -0,0 +1,18 @@
+'''
+Created on 20 Jan 2016
+
+@author: time2
+'''
+import unittest
+
+
+class TestDiscordBot(unittest.TestCase):
+
+
+    def test_init(self):
+        pass
+    
+
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.testName']
+    unittest.main()
